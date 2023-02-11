4d54226ada0d4acf1aec7119177b48141ec08a51
Ali Öztürk <84031734+traaz@users.noreply.github.com>
2023-02-12 00:28:16 +0300

    category and routing

---
+++ frontend/frontend/angular-ecommerce/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { HttpClientModule} from '@angular/common/http';
+import { ProductService } from './services/product.service';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+
+const routes:Routes = [
+  {path:'category/:id', component:ProductListComponent},
+  {path:'category', component:ProductListComponent},
+  {path:'products', component:ProductListComponent},
+  {path:'', redirectTo:'/products', pathMatch:'full'}, //bir sey verilmezzse productsa git
+  {path:'**', redirectTo:'/products', pathMatch:'full'},//hicbirseyle eslemezey yine products
+
+
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ProductCategoryMenuComponent
+  ],
+  imports: [
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [ProductService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- spring-boot-ecommerce/src/main/java/com/ali/springbootecommerce/config/MyDataRestConfig.java
+++ spring-boot-ecommerce/src/main/java/com/ali/springbootecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,29 @@ package com.ali.springbootecommerce.config;
 
 import com.ali.springbootecommerce.entity.Product;
 import com.ali.springbootecommerce.entity.ProductCategory;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] theUnSupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
@@ -22,5 +37,26 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnSupportedActions)))
                 .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnSupportedActions)));
+        exposeId(config);
+   
+    }
+
+    private void exposeId(RepositoryRestConfiguration config) {
+        //tum entityleri listeye alcaz entitymanagerden
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+
+        for (EntityType item : entities){
+            entityClasses.add(item.getJavaType());
+            System.out.println(item.getName());//ProductCategory, Product
+        }
+        Class[] domainTypes = entityClasses.toArray(new Class[0]); //We need to convert the ArrayList to an array of Class objects
+        //Class[0], we are telling the arraylist to convert to an array of Class[]. We make use of Class[0] for efficiency purposes
+        System.out.println(domainTypes[0]);//class com.ali.springbootecommerce.entity.ProductCategory
+        System.out.println(domainTypes[1]);//class com.ali.springbootecommerce.entity.Product
+        config.exposeIdsFor(domainTypes); //idsini ekledik api'ye hem product'in hem productcategorinin
+
+
     }
 }
